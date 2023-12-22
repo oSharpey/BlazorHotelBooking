4cb552dd108a78f4b02db06d1f792d7a0624eee6
Oscar Sharpe <oscarsharpe@proton.me>
2023-12-22 14:36:19 +0000

    Display Hotels to user using local API with no DB

---
+++ BlazorHotelBooking/Server/Controllers/HotelController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using BlazorHotelBooking.Shared;
+
+
+namespace BlazorHotelBooking.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        public async Task<ActionResult<List<Hotel>>> GetAllHotels()
+        {
+            var list = new List<Hotel>
+            {
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Hilton London Hotel",
+                    SBPrice = 375,
+                    DBPrice = 775,
+                    FamPrice = 950,
+                    Spaces = 20
+                },
+                
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "London Marriott Hotel",
+                    SBPrice = 300,
+                    DBPrice = 500,
+                    FamPrice = 900,
+                    Spaces = 20
+                },
+                
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Travelodge Brighton Seafront",
+                    SBPrice = 80,
+                    DBPrice = 120,
+                    FamPrice = 150,
+                    Spaces = 20
+                },
+
+                new Hotel
+                {
+                    Id = 4,
+                    Name = "Kings Hotel Brighton",
+                    SBPrice = 180,
+                    DBPrice = 400,
+                    FamPrice = 520,
+                    Spaces = 20
+                },
+
+                new Hotel
+                {
+                    Id = 5,
+                    Name = "Leonardo Hotel Brighton",
+                    SBPrice = 180,
+                    DBPrice = 400,
+                    FamPrice = 520,
+                    Spaces = 20
+                },
+
+                new Hotel
+                {
+                    Id = 6,
+                    Name = "Nevis Bank Inn, Fort William",
+                    SBPrice = 90,
+                    DBPrice = 100,
+                    FamPrice = 155,
+                    Spaces = 20
+                },
+
+
+            };
+            return Ok(list);
+        }
+
+    }
+}
