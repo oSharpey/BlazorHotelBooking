c20e169de1cca91a8b430b493fa99e629a7d9020
Oscar Sharpe <oscarsharpe@proton.me>
2023-12-22 20:09:41 +0000

    implement add, edit and delete functions

--- BlazorHotelBooking/Server/Controllers/HotelController.cs
+++ BlazorHotelBooking/Server/Controllers/HotelController.cs
@@ -27,5 +27,66 @@ namespace BlazorHotelBooking.Server.Controllers
             return Ok(list);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hotel>> GetHotel(int id)
+        {
+            var dbhotel = await _context.Hotels.FindAsync(id);
+
+            if (dbhotel == null)
+            {
+                return NotFound("This hotel does not exist");
+            }
+
+            return Ok(dbhotel);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Hotel>>> AddHotel(Hotel hotl)
+        {
+            _context.Hotels.Add(hotl);
+            await _context.SaveChangesAsync();
+
+            return await GetAllHotels();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Hotel>>> UpdateHotel(int id, Hotel hotl)
+        {
+            var dbHotel = await _context.Hotels.FindAsync(id);
+
+            if (dbHotel == null)
+            {
+                return NotFound("This hotel does not exist");
+            }
+
+            dbHotel.Id = hotl.Id;
+            dbHotel.Name = hotl.Name;
+            dbHotel.SBPrice = hotl.SBPrice;
+            dbHotel.DBPrice = hotl.DBPrice;
+            dbHotel.FamPrice = hotl.FamPrice;
+            dbHotel.Spaces = hotl.Spaces;
+            dbHotel.Description = hotl.Description;
+
+            _context.Hotels.Add(hotl);
+            await _context.SaveChangesAsync();
+
+            return await GetAllHotels();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Hotel>>> DeleteHotel(int id)
+        {
+            var dbHotel = await _context.Hotels.FindAsync(id);
+
+            if (dbHotel == null)
+            {
+                return NotFound("This hotel does not exist");
+            }
+
+            _context.Hotels.Remove(dbHotel);
+            await _context.SaveChangesAsync();
+
+            return await GetAllHotels();    
+        }
     }
 }
