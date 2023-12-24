d24d29c427f139a421b296eb51d146f9ff54cd5a
Oscar Sharpe <oscarsharpe@proton.me>
2023-12-24 17:39:53 +0000

    Create Authentication Service

---
+++ BlazorHotelBooking/Client/Service/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using BlazorHotelBooking.Shared.Models;
+
+namespace BlazorHotelBooking.Client.Service
+{
+    public interface IAuthService
+    {
+        Task<RegisterResult> Register(RegisterModel registerModel);
+        Task<LoginResult> Login(LoginModel loginModel);
+        Task Logout();
+
+    }
+}
