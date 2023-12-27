9685a5ba2c9b3b42aebf47344c60cb050c04845f
Oscar Sharpe <oscarsharpe@proton.me>
2023-12-27 15:45:10 +0000

    Revert "Revert "removed useremail from booking table as can be found from userId""
    This reverts commit 959db7c18f99f3ba7d862a77f3cf7e1ebd7a435c.

--- BlazorHotelBooking/Server/Migrations/20231227154219_hotelbooking3.Designer.cs
+++ BlazorHotelBooking/Server/Migrations/20231227154219_hotelbooking3.Designer.cs
@@ -4,6 +4,7 @@ using BlazorHotelBooking.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorHotelBooking.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231227154219_hotelbooking3")]
+    partial class hotelbooking3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
