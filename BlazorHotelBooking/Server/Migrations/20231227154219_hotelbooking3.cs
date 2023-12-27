f350d70a69f53127f5d962d228b63303439c279c
Oscar Sharpe <oscarsharpe@proton.me>
2023-12-27 15:44:06 +0000

    removed useremail from booking table as can be found from userId

---
+++ BlazorHotelBooking/Server/Migrations/20231227154219_hotelbooking3.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorHotelBooking.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class hotelbooking3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserEmail",
+                table: "HotelBookings");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "a2e7fc4e-1499-42d9-a9e4-80a82b2378a6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "9a5f784b-4320-4b52-ac82-60f34b14c67b");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserEmail",
+                table: "HotelBookings",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "c49239c0-a656-4669-8ec7-0c99fd2676ea");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "e4843ecb-86a4-43e6-8625-b01f0378b555");
+        }
+    }
+}
