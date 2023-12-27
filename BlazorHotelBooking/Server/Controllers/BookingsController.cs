79bea260b78f0efdc963d4a857df7cd1d88e06d5
Oscar Sharpe <oscarsharpe@proton.me>
2023-12-27 18:50:06 +0000

    Added the mybookings page to show the users bookings related to them

--- BlazorHotelBooking/Server/Controllers/BookingsController.cs
+++ BlazorHotelBooking/Server/Controllers/BookingsController.cs
@@ -1,9 +1,11 @@
 ﻿using BlazorHotelBooking.Server.Data;
 using BlazorHotelBooking.Shared;
+using BlazorHotelBooking.Shared.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace BlazorHotelBooking.Server.Controllers
 {
@@ -22,22 +24,24 @@ namespace BlazorHotelBooking.Server.Controllers
         }
 
         [HttpGet("hotel/userbooking")]
-        public async Task<ActionResult<List<HotelBooking>>> GetAllBookingsWithId(string userId)
+        public async Task<ActionResult<List<HotelBookingViewModel>>> GetAllBookingsWithId(string userId)
         {
             var query = from hotel in _context.Hotels
                         join booking in _context.HotelBookings on hotel.Id equals booking.HotelId
                         where booking.UserId == userId
-                        select new { 
-                            hotel.Name, 
-                            booking.BookingDate, 
-                            booking.CheckIn, 
-                            booking.CheckOut, 
-                            booking.NumberOfNights, 
-                            booking.TotalPrice, 
-                            booking.DepositAmountPaid 
+                        select new HotelBookingViewModel { 
+                            hotelName = hotel.Name,
+                            RoomType = booking.RoomType,  
+                            CheckIn = booking.CheckIn,  
+                            CheckOut = booking.CheckOut,
+                            NumberOfNights = booking.NumberOfNights,  
+                            TotalPrice = booking.TotalPrice,
+                            DepositAmountPaid = booking.DepositAmountPaid,
+                            BookingDate = booking.BookingDate
                         };
+            var result = await query.ToListAsync();
 
-            return Ok(query);
+            return Ok(result);
         }
 
 
@@ -48,7 +52,7 @@ namespace BlazorHotelBooking.Server.Controllers
             _context.HotelBookings.Add(hotl);
             await _context.SaveChangesAsync();
 
-            return await GetAllBookingsWithId(hotl.UserId);
+            return Ok("Booking Successful");
         }
     }
 }
