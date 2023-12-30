bf91556656d97308686dc553a34ac1183870c53c
Oscar Sharpe <oscarsharpe@proton.me>
2023-12-30 16:50:58 +0000

    Created tour class

---
+++ BlazorHotelBooking/Shared/Tour.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHotelBooking.Shared
+{
+    internal class Tour
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2")]
+        public decimal Cost { get; set; }
+        [Column(TypeName = "decimal(18,2")]
+        public int DurationInDays { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
