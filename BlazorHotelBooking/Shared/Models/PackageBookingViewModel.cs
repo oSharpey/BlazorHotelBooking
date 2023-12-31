5fe2458a12f97ae271be4b33fd97f2162e0eec45
Oscar Sharpe <oscarsharpe@proton.me>
2023-12-31 16:39:30 +0000

    added api routes to book package

---
+++ BlazorHotelBooking/Shared/Models/PackageBookingViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHotelBooking.Shared.Models
+{
+    public class PackageBookingViewModel
+    {
+        public string bookingId { get; set; }
+        public string TourName { get; set; }
+        public DateTime CommencementDate{ get; set; } 
+        public DateTime EndDate { get; set; }
+        public int NumberOfGuests { get; set; }
+        public string hotelName { get; set; }
+        public string RoomType { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public int NumberOfNights { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal DepositAmountPaid { get; set; }
+        public DateTime BookingDate { get; set; } 
+        public bool paidInfull { get; set; }
+        public bool IsCancelled { get; set; }
+        public DateTime PaymentDueDate { get; set; }
+
+
+        
+
+    }
+}
