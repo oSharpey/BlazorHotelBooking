d647606e8038771c5b762d17c20bfaab84746bd1
Oscar Sharpe <oscarsharpe@proton.me>
2024-01-01 17:13:04 +0000

    added page to let users see payments they have made

---
+++ BlazorHotelBooking/Server/Controllers/PaymentController.cs
@@ -0,0 +1,40 @@
+﻿using BlazorHotelBooking.Server.Data;
+using BlazorHotelBooking.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorHotelBooking.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public PaymentController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<List<Payments>>> GetAllPayments()
+        {
+            var list = await _context.Payments.OrderByDescending(x => x.PaymentDate).ToListAsync();
+
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<ActionResult<List<Payments>>> GetAllPaymentsByUserId(string id)
+        {
+            var list = await _context.Payments.Where(x => x.UserId == id).OrderByDescending(x => x.PaymentDate).ToListAsync();
+
+            return Ok(list);
+        }
+    }
+}
