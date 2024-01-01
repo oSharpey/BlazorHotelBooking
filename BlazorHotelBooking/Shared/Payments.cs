eca517fb691b4834a2be9286e749ce298d125f54
Oscar Sharpe <oscarsharpe@proton.me>
2024-01-01 14:35:03 +0000

    created payment class and added payment table to database

---
+++ BlazorHotelBooking/Shared/Payments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHotelBooking.Shared
+{
+    public class Payments
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string UserId { get; set; }
+        public string bookingId { get; set; }
+        public string bookingType { get; set; }
+        public DateTime PaymentDate { get; set; } = DateTime.Now.Date;
+        public decimal AmountPaid { get; set; }
+    }
+}
